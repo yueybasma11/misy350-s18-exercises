939192ff1c851a22023a9b5ff179b2878905c053
yueybasma11 <yuey@udel.edu>
2018-04-19 10:24:04 -0400

    Python

---
+++ 4-19-18/python-basics.py
@@ -0,0 +1,10 @@
+*print "Hello World!"
+greeting = "Hello World"
+
+print greeting #this prints out a string
+print len(greeting)
+print greeting.upper()
+print greeting[4] #returns
+print greeting + " " + name "!"
+#print "%s %s!" % (greeting, name) Expects 2 strings, only gets 1
+print "%s %s! How are you doing, today?" % (greeting, name)
---
+++ 4-5-2018/my-code.js
@@ -0,0 +1,7 @@
+let blist = ["end of the semester", "no research", "Disen gets better"];
+console.log(blist(2));
+blist.push("get a beer");
+console.log(blist.length);
+
+console.log(blist.length);
+console.log(blist);
