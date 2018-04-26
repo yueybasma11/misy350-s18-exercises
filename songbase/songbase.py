c49dd37ccf43b931f8ffe3f2b47975fd62933339
yueybasma11 <yuey@udel.edu>
2018-04-26 10:40:51 -0400

    Songbase

---
+++ songbase/songbase.py
@@ -0,0 +1,18 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "hello"
+if __name__ == '__main__':
+    app.run()
+
+
+@app.route("/user")
+def user():
+    return "this is the page for users"
+
+
+if __name__ =='__main__':
+    app.run()
