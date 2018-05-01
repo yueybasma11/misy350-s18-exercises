d73f9b29e1a7bf880930a426a050da3682b913e4
yueybasma11 <yuey@udel.edu>
2018-05-01 10:40:12 -0400

    templates

--- songbase/songbase.py
+++ songbase/songbase.py
@@ -1,18 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return "hello"
+    return "hello Yusef Basma"
 if __name__ == '__main__':
     app.run()
 
 
 @app.route("/user")
 def user():
-    return "this is the page for users"
+    #return "this is the page for users"
+    return render_template("index.html")
 
 
-if __name__ =='__main__':
-    app.run()
+@app.route('/users/<string:username>')
+def users(username):
+    #return "<h1>Hello %s<h1>" % username
+    return render_template('user.html',uname=username)
+
+    if __name__ =='__main__':
+        app.run()
